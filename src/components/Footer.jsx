cb793261b343cd99c8f502c2d6737a990ca59c7b
sebastianpitre <sebastianandrespitrediaz@gmail.com>
2025-06-15 02:59:37 -0500

    Actualizar el enlace en el pie de página para redirigir a la nueva URL de Ocloud.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     <footer className="text-center text-lg-start">
       <div className="text-center p-3 mb-5">
       
-        Tecnología de <a className="text-info text-bold" href="https://frontiend.vercel.app/" target="_blank"> <img className="mt-n1" src="/logo3.png" width={30} alt="" />loud. </a> © 2025 
+        Tecnología de <a className="text-info text-bold" href="https://ocloud.vercel.app/" target="_blank"> <img className="mt-n1" src="/logo3.png" width={30} alt="" />loud. </a> © 2025 
         
       </div>
     </footer>
